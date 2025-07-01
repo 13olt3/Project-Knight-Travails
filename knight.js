6f34e1d208c4259852723064720879e6cccd0a43
Thanh Nguyen <ehh_13olt3@hotmail.com>
2025-07-01 20:53:23 +1000

    Tried a board loop

--- knight.js
+++ knight.js
@@ -12,41 +12,50 @@ function knightMoves(startPos, endPos) {
     }
   }
 }
+function buildBoard() {
+  let board = [
+    [[], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], []],
+    [[], [], [], [], [], [], [], []],
+  ];
 
-class Node {
-  constructor(a, b) {
-    this.position = [a, b];
+  for (let i = 0; i <= 7; ++i) {
+    for (let j = 0; j <= 7; ++j) {
+      let a = j,
+        b = i;
+
+      if (a + 2 <= 7 && b + 1 <= 7) {
+        board[i][j].push([a + 2, b + 1]);
+      }
+      if (a + 1 <= 7 && b + 2 <= 7) {
+        board[i][j].push([a + 1, b + 2]);
+      }
+      if (a + 2 <= 7 && b - 1 >= 0) {
+        board[i][j].push([a + 2, b - 1]);
+      }
+      if (a + 1 <= 7 && b - 2 >= 0) {
+        board[i][j].push([a + 1, b - 2]);
+      }
+      if (a - 1 >= 0 && b - 2 >= 0) {
+        board[i][j].push([a - 1, b - 2]);
+      }
+      if (a - 2 >= 0 && b - 1 >= 0) {
+        board[i][j].push([a - 2, b - 1]);
+      }
+      if (a - 1 >= 0 && b + 2 <= 7) {
+        board[i][j].push([a - 1, b + 2]);
+      }
+      if (a - 2 >= 0 && b + 1 <= 7) {
+        board[i][j].push([a - 2, b + 1]);
+      }
+    }
   }
-  possibleMoves() {
-    let a = this.position[0];
-    let b = this.position[1];
-    let possibleMoves = [];
-    if (a + 1 <= 7 && b + 2 <= 7) {
-      possibleMoves.push(new Node(a + 1, b + 2));
-    }
-    if (a + 1 <= 7 && b - 2 >= 0) {
-      possibleMoves.push(new Node(a + 1, b - 2));
-    }
-    if (a + 2 <= 7 && b + 1 <= 7) {
-      possibleMoves.push(new Node(a + 2, b + 1));
-    }
-    if (a + 2 <= 7 && b - 1 >= 0) {
-      possibleMoves.push(new Node(a + 2, b - 1));
-    }
-    if (a - 1 >= 0 && b + 2 <= 7) {
-      possibleMoves.push(new Node(a - 1, b + 2));
-    }
-    if (a - 1 >= 0 && b - 2 >= 0) {
-      possibleMoves.push(new Node(a - 1, b - 2));
-    }
-    if (a - 2 >= 0 && b + 1 <= 7) {
-      possibleMoves.push(new Node(a - 2, b + 1));
-    }
-    if (a - 2 >= 0 && b - 1 >= 0) {
-      possibleMoves.push(new Node(a - 2, b - 1));
-    }
-    return possibleMoves;
-  }
+  console.log(board[3][3]);
 }
 
-knightMoves([5, 5], [1, 3]);
+buildBoard();
